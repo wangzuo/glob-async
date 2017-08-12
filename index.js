69553eab6af0c29af496b93afeb8c198cc2aeaae
Wang Zuo <wzuoadjusted@gmail.com>
2017-08-12 14:07:27 +0800

    add options

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const glob = require('glob');
 
-const globAsync = async pattern =>
+const globAsync = (pattern, options) =>
   new Promise((resolve, reject) => {
-    glob(pattern, (err, files) => {
+    glob(pattern, options, (err, files) => {
       if (err) return reject(err);
       resolve(files);
     });
--- test.js
+++ test.js
@@ -2,4 +2,5 @@ const glob = require('./');
 
 test('glob', async () => {
   expect(await glob('*.js')).toEqual(['index.js', 'test.js']);
+  expect(await glob('*.json')).toEqual(['package.json']);
 });
