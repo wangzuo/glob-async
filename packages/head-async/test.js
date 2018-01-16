bd52da1c6667cc53a96aa1a2e562abdd91896e2b
Wang Zuo <wzuoadjusted@gmail.com>
2018-01-16 11:03:55 +0800

    update test

--- packages/head-async/test.js
+++ packages/head-async/test.js
@@ -3,6 +3,7 @@ const head = require('./');
 test('count', async () => {
   expect(await head('./file.txt')).toMatchSnapshot();
   expect(await head('./file.txt', { count: 3 })).toMatchSnapshot();
+  expect(await head('./file.txt', { count: 100 })).toMatchSnapshot();
 });
 
 test('bytes', async () => {
