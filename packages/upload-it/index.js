cfbfd5cc975f3650184c498ca5aa4f3e77f2792d
Wang Zuo <wzuoadjusted@gmail.com>
2018-02-01 11:57:07 +0800

    uploadRemote

--- packages/upload-it/index.js
+++ packages/upload-it/index.js
@@ -21,6 +21,31 @@ function filehash(filepath, secret) {
   });
 }
 
+async function uploadRemote(url, options = {}) {
+  const storage = Storage({ projectId: options.projectId });
+  const bucket = storage.bucket(options.bucket);
+  const hmac = crypto.createHmac('sha1', options.secret);
+  const digest = hmac.update(url).digest('hex');
+  const m1 = path.extname(url).match(/(\.[a-zA-Z0-9]+)/);
+  if (!m1) throw new Error(`Cannot find ext for ${url}`);
+
+  const filename = `${digest}${m1[1]}`;
+  const contentType = mime.getType(filename);
+
+  return new Promise(function(resolve, reject) {
+    const stream = bucket.file(filename).createWriteStream({
+      metadata: { contentType }
+    });
+
+    request.get(url).pipe(stream);
+
+    stream.on('error', reject);
+    stream.on('finish', function() {
+      resolve(filename);
+    });
+  });
+}
+
 async function uploadLocal(filepath, options = {}) {
   const storage = Storage({ projectId: options.projectId });
   const bucket = storage.bucket(options.bucket);
@@ -47,9 +72,9 @@ async function uploadLocal(filepath, options = {}) {
 }
 
 function upload(options = {}) {
-  return async function(src) {
+  return async function(str) {
     if (str.match(/^http/)) {
-      // todo
+      return await uploadRemote(str, options);
     } else {
       return await uploadLocal(str, options);
     }
